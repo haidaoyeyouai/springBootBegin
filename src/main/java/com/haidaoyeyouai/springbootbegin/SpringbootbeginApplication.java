7746ce3ac58c0c224eac8e82841889cc5ee49354
jwz <jwz>
2019-08-31 22:48:47 +0800

    xxxxx

---
+++ src/main/java/com/haidaoyeyouai/springbootbegin/SpringbootbeginApplication.java
@@ -0,0 +1,13 @@
+package com.haidaoyeyouai.springbootbegin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootbeginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootbeginApplication.class, args);
+	}
+
+}
